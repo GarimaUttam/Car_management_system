2ecc0f0afc315deb7885cac89558def842ee1120
GarimaUttam <garimauttam191203@gmail.com>
2025-01-14 13:28:27 +0530

    added engine validation functin in the model folder

--- models/engine.go
+++ models/engine.go
@@ -1,10 +1,45 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
 
 type Engine struct {
 	EngineID      uuid.UUID `json:"engine_id`
 	Displacement  int64     `json:"displacement"`
 	NoOfCylinders int64     `json:"noOfCylinders"`
 	CarRange      int64     `json:"carRange"`
+}
+
+func  ValidateEngineRequest(EngineReq EngineRequest) error {
+	if err := validateDisplacement(EngineReq.Displacement); err != nil {
+		return err
+	}
+	if err := validateNoOfCylinders(EngineReq.noOfCylinders); err != nil {
+		return err
+	}
+	if err := validateCarRange(EngineReq.carRange); err != nil {
+		return err
+	}
+	return nil
+}
+func validateDisplacement(displacement int64) error {
+	if displacement <= 0 {
+		return errors.New("displacement must be greater than zero")
+	}
+	return nil
+}
+func validateNoOfCylinders(noOfCylinders int64) error {
+	if noOfCylinders <= 0 {
+		return errors.New("noOfCylinders must be greater than zero")
+	}
+	return nil
+}
+func validateCarRange(carRange int64) error {
+	if carRange <= 0 {
+		return errors.New("carRange must be greater than zero")
+	}
+	return nil
 }
