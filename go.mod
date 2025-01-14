01f6ac61713c24e99d97263ec62028babc8afebe
GarimaUttam <garimauttam191203@gmail.com>
2025-01-14 10:43:03 +0530

    added car valiate function in models folder

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/GarimaUttam/Car_management_system
 
 go 1.23.4
+
+require github.com/google/uuid v1.6.0 // indirect
--- models/car.go
+++ models/car.go
@@ -1,1 +1,126 @@
 package models
+
+import (
+	"errors"
+	"strconv"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Car struct {
+	ID        uuid.UUID `json:"id`
+	Name      string    `json:"name"`
+	Year      string    `json:"year"`
+	Brand     string    `json:"brand"`
+	FuelType  string    `json:"fuel_type"`
+	Engine    Engine    `json:"engine"`
+	Price     float64   `json:"price"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	
+}
+
+type CarRequest struct {
+	Name      string    `json:"name"`
+	Year      string    `json:"year"`
+	Brand     string    `json:"brand"`
+	FuelType  string    `json:"fuel_type"`
+	Engine    Engine    `json:"engine"`
+	Price     float64   `json:"price"`
+}
+func ValidateRequest(carReq CarRequest) error {
+	if err := validateName(carReq.Name); err != nil {
+		return err
+	}
+	if err := validateYear(carReq.Year); err != nil {
+		return err
+	}
+	if err := validatedBrand(carReq.Brand); err != nil {
+		return err
+	}
+	if err := validateFuelType(carReq.FuelType); err != nil {
+		return err
+	}
+	if err := validateEngine(carReq.Engine); err != nil {
+		return err
+	}
+	if err := validatePrice(carReq.Price); err != nil {
+		return err
+	}
+	return nil
+}
+
+
+
+func validateName(name string) error{
+	if name == ""{
+		return errors.New("Name is Required")
+	}
+	return nil
+}
+func validateYear(year string) error {
+	if year == "" {
+		return errors.New("Year is required")
+	}
+	_, err := strconv.Atoi(year)
+	if err != nil {
+		return errors.New("Year must be a valid number")
+	}
+	currentYear := time.Now().Year()
+	yearInt, _ := strconv.Atoi()
+	if yearInt < 1886 || yearInt > currentYear {
+		return errors.New("Year must me between 1886 and current year")
+	}
+	return nil
+}
+
+func validatedBrand(brand string) error {
+	if brand == "" {
+		return errors.New("Brand is required")
+	}
+	return nil
+}
+
+func validateFuelType(fuelType string) error{
+	validateFuelTypes := []string{"Persol","Diesel","Electric","Hybrid"}
+	for _,validType := range validateFuelTypes {
+		if fuelType == validType {
+			return nil
+		}
+	}
+	return errors.New("FeulType must be one of : Petrol, Diesel, Electric, Hybrid")
+ }
+
+ func validateEngine(engine Engine) error {
+	if engine.EngineID == uuid.Nil {
+		return errors.New("EngineId is required")
+	}
+	if engine.Displacement <= 0 {
+		return errors.New("displacement must be greater than zero")
+	}
+	if engine.NoOfCylinders <= 0 {
+		return errors.New("noOfCylinders must be greater than zero")
+	}
+	if engine.NoOfCylinders<= 0{
+		return errors.New("carRange must be greater than zero")
+	}
+	return nil
+ }
+
+ func validatePrice(price float64) error {
+	if price <= 0 {
+		return errors.New("Price must be greater than zero")
+	}
+	return nil
+ }
+
+
+
+
+
+
+
+
+
+
